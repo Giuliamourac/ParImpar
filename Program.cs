387eabd88210d49603843e969d4e6cdb34d8523b
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-05-12 10:20:31 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um numero para saber se ele e par ou impar:");
+            int numero = Convert.ToInt32(Console.ReadLine()!);
+
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar!");
+            }
+
+        }
+    }
+}
